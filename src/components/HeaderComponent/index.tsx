a6185069cd9e21b6b1c1b18ea5014375d714eca2
Dhayana <dhayanadesenvolvimento@gmail.com>
2020-08-28 22:17:06 -0300

    ajuste no input do cabecalho

--- src/components/HeaderComponent/index.tsx
+++ src/components/HeaderComponent/index.tsx
@@ -1,31 +1,28 @@
 import React from 'react'
-import { Header } from './styles'
-import { FaGithub, FaBell } from 'react-icons/fa'
+import { Header, ContainerBell, Navigation, InputContainer, List, GitHubIcon, BellIcon } from './styles'
 
 const HeaderComponent:React.FC = () => {
     return (
         <Header>
-            <figure title="Ícone github">
-                <FaGithub />
-            </figure>
+            <GitHubIcon title="Ícone github" />
 
-            <nav>
-                <div>
+            <Navigation>
+                <InputContainer>
                     <input placeholder="Search or jump to..." type="text"/>
                     <figure>/</figure>
-                </div>
-                <ul>
+                </InputContainer>
+
+                <List>
                     <li><a href="/">Pull requests</a></li>
                     <li><a href="/">Issues</a></li>
                     <li><a href="/">Marketplace</a></li>
                     <li><a href="/">Explore</a></li>
-                </ul>
-            </nav>
+                </List>
+            </Navigation>
 
-            <figure title="Notificacões">
-                <FaBell />
-            </figure>
-
+            <ContainerBell title="Notificacões">
+                <BellIcon />
+            </ContainerBell>
         </Header>
     )
 }
--- src/components/HeaderComponent/styles.ts
+++ src/components/HeaderComponent/styles.ts
@@ -1,7 +1,22 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { FaGithub, FaBell } from 'react-icons/fa'
 
 export const Header = styled.header`
+    width: 100%;
+    padding: 8px 16px;
+    background-color: var(--black);
+    
+    display: flex;
+    align-items: center;
+`
 
+export const Navigation = styled.nav`
+    width: 100%;
+    display: flex;
+    align-items: center;
+`
+
+export const InputContainer = styled.div`
     input::placeholder {
         color: var(--white-text);
     }
@@ -13,111 +28,137 @@ export const Header = styled.header`
     input::-ms-input-placeholder { 
         color: var(--white-text);
     }
-
-    width: 100%;
-    padding: 8px 16px;
-    background-color: var(--black);
-    
+   
     display: flex;
     align-items: center;
 
-    > figure:last-child {
-        cursor: pointer;
-        position: relative;
-        flex-shrink: 0;
+    margin-right: 16px;
+    border-radius: 5px;
 
-        font-size: 20px;
+    color: var(--white-text);
+    background-color: var(--grey);
+    opacity: 0.7;
+    width:  289px;
+    transition: width 0.2s linear;
 
-        &::before {
-            content: "";
-            width: 10px;
-            height: 10px;
-            border-radius: 50%;
-            background-color: var(--span);
-            
-            position: absolute;
-            top: 0;
-            right: 0;
+    input {
+        color: inherit;
+        border: none;
+        height: 30px;
+        width: 245px;
+        padding: 0 12px;
+        background-color: transparent;
+        transition: width 0.2s linear;
+    }
+
+    figure {
+        width: 20px;
+        height: 20px;
+        margin: 0 12px;
+        line-height: 20px;
+        text-align: center;
+        border-radius: 5px;
+        border: 1px solid var(--white);
+    }
+   
+    &:focus-within figure {
+        display: none;
+    }
+
+    &:focus-within input {
+        color: var(--black-text);
+        width: 100%;
+    }
+
+    &:focus-within input::placeholder {
+        color: var(--black-text);
+    }
+
+    &:focus-within input:-ms-input-placeholder {
+        color: var(--black-text);
+    }
+
+    &:focus-within input::-ms-input-placeholder { 
+        color: var(--black-text);
+    } 
+
+    &:focus-within {
+        background-color: var(--white);
+        opacity: 1;
+        width: 500px;
+    }
+
+    @media (max-width: 950px) {
+        &:focus-within {
+            width: 100%;
         }
     }
 
-    > figure {
-        cursor: pointer;
-        margin-right: 16px;
+`
 
-        color: var(--white);
-        font-size: 30px;
-        border-radius: 50%;
-
+export const List = styled.ul`
+    list-style: none;
+    display: flex;
+    flex-shrink: 0;
+  
+    li {
+        color: var(--white-text);
         transition: opacity 0.4s;
-        flex-shrink: 0;
+        margin-right: 16px;
+        font-weight: bold;
+        font-size: 14px;
 
         &:hover {
             opacity: 0.6;
         }
-    }
 
-    nav {
-        width: 100%;
-        display: flex;
-        align-items: center;
-
-        div {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-
-            margin-right: 16px;
-            border-radius: 5px;
-            color: var(--white-text);
-            background-color: var(--grey);
-            opacity: 0.7;
-
-            input {
-                color: inherit;
-                border: none;
-                height: 30px;
-                width: 200px;
-                padding: 0 12px;
-                background-color: transparent;
-                transition: width 0.4s;
-            }
-
-            input:focus {
-                width: 350px;
-            }
-
-            figure {
-                width: 20px;
-                height: 20px;
-                margin: 0 12px;
-                text-align: center;
-                border-radius: 5px;
-                border: 1px solid var(--white);
-            }
-        }
-
-        ul {
-            list-style: none;
-            display: flex;
-
-            li {
-                color: var(--white-text);
-                transition: opacity 0.4s;
-                margin-right: 16px;
-                font-weight: bold;
-                font-size: 14px;
-
-                &:hover {
-                    opacity: 0.6;
-                }
-
-                a {
-                    color: inherit;
-                    text-decoration: none;
-                }
-            }
+        a {
+            color: inherit;
+            text-decoration: none;
         }
     }
+`
 
+export const ContainerBell = styled.figure`
+    position: relative;
+    width: 20px;
+
+    &::before {
+        content: "";
+        width: 10px;
+        height: 10px;
+        z-index: 1;
+
+        opacity: 1 !important;
+        border-radius: 50%;
+        background-color: var(--span);
+        
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+`
+
+export const icon = css`
+    cursor: pointer;
+    margin-right: 16px;
+    color: var(--white);
+    border-radius: 50%;
+
+    transition: opacity 0.4s;
+    flex-shrink: 0;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
+
+export const BellIcon = styled(FaBell)`
+    ${icon}
+    font-size: 20px;
+`
+
+export const GitHubIcon = styled(FaGithub)`
+    ${icon}
+    font-size: 30px;
 `
